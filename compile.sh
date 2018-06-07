080380c3f0426a4a894df05dcd44ef5ba0543a52
FluorineDog <fluorine@gmail.com>
2018-06-07 13:02:35 +0800

    update

--- compile.sh
+++ compile.sh
@@ -2,4 +2,4 @@
 python3 lexer_gen.py --root_dir . --output_dir handout || exit
 bison handout/parser.yxx --defines=handout/parser.hxx --output=handout/parser.cpp || exit
 flex++ -o handout/lexer.cpp handout/lexer.l || exit
-# g++ handout/*.cpp kernel/*.cpp --std=c++17 --Wall --Wno-unused || exit
+g++ handout/*.cpp --std=c++17 -Wall -Wno-unused || exit
