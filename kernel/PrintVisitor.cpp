5ca9d042554b48fa75b09c2a0be16c781ce98ac5
FluorineDog <fluorine@gmail.com>
2018-06-08 00:59:48 +0800

    rm bb

--- kernel/PrintVisitor.cpp
+++ kernel/PrintVisitor.cpp
@@ -1,17 +1,1 @@
-#include "generated/PrintVisitor.h"
-#include "internal.h"
-void PrintVisitor::visit(class Integer* node) { 
-  cout << node->num; 
-}
-void PrintVisitor::visit(class BinaryExpr* node) {
-  node->left->accept(*this);
-  cout << "<"  << node->op << ">";
-  node->right->accept(*this);
-}
-void PrintVisitor::visit(class Block* node) {
-  cout << "stmts" << node->stmts.size() << endl;
-  for (auto& s : node->stmts) {
-    s->accept(*this);
-    cout << endl;
-  }
-}
+generated/template/PrintVisitor.cpp
