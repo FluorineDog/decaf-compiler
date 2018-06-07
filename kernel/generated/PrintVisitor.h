9c0bc6f8c967d1995c4885219e39b33711700204
FluorineDog <fluorine@gmail.com>
2018-06-08 00:52:33 +0800

    git rm generated

--- kernel/generated/PrintVisitor.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "common.h"
-#include "Visitor.h"
-class PrintVisitor : public Visitor{
-  virtual void visit(class Integer* node);
-  virtual void visit(class BinaryExpr* node);
-  virtual void visit(class Block* node);
-
-};
--- kernel/generated/Visitor.1.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-class Visitor {
- public:
-  virtual void visit(class Integer* node) = 0;
-  virtual void visit(class BinaryExpr* node) = 0;
-  virtual void visit(class Block* node) = 0;
-};
-
-#include "PrintVisitor.h"
