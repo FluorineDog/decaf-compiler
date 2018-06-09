a6b9ff631ff610a530a617603705f1e50ebdd926
FluorineDog <fluorine@gmail.com>
2018-06-09 15:11:39 +0800

    update

--- parser/bnf.hs
+++ parser/bnf.hs
@@ -6,12 +6,11 @@ Type ::= int | double | bool | string | ident | Type [ ]
 TypeVoid ::= Type | void 
 FunctionDecl ::= TypeVoid ident ( Formals ) StmtBlock
 Formals ::= Variable,*
-ClassDecl ::= class ident <extends ident> < implements ident,* > { Field* }
+ClassDecl ::= class ident < extends ident > < implements ident,* > { Field* }
 Field ::= VariableDecl | FunctionDecl
 InterfaceDecl ::= interface ident { Prototype* }
+Prototype ::= TypeVoid ident ( Formals ) 
+StmtBlock ::= { VariableDecl* Stmt* }
+Stmt ::= < Expr > ; | IfStmt | WhileStmt | ForStmt | BreakStmt | ReturnStmt | PrintStmt | StmtBlock 
+IfStmt ::=  if ( Expr ) Stmt < else Stmt > 
 
-
-Field
-InterfaceDecl
-Prototype
-StmtBlock
