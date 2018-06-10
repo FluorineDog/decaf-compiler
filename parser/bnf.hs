761d107a07773ac38da1cbebab13d7dff951102b
FluorineDog <fluorine@gmail.com>
2018-06-10 13:51:09 +0800

    conflicts solved

--- parser/bnf.hs
+++ parser/bnf.hs
@@ -1,20 +1,21 @@
 Program ::= Decl+
 ident ::= ID
+type_ident ::= TYPE_ID
 Decl ::= VariableDecl | FunctionDecl | ClassDecl | InterfaceDecl
 VariableDecl ::= Variable ;
 Variable ::= Type ident
 
-Type ::= T_int | T_double | T_bool | T_string | TYPE_ID | Type [ ]
-TypeVoid ::= Type | T_void 
-FunctionDecl ::= TypeVoid ident ( Formals ) StmtBlock
+Type ::= T_int | T_double | T_bool | T_string | type_ident | Type [ ]
+FunctionDecl ::= Type ident ( Formals ) StmtBlock | \
+ T_void ident ( Formals ) StmtBlock
 Formals ::= Variable,*
-ClassDecl ::= T_class ident Extender? Implementor? { Field* }
-Extender ::= T_extends ident
-Implementor ::= T_implements ident?
+ClassDecl ::= T_class type_ident Extender? Implementor? { Field* }
+Extender ::= T_extends type_ident
+Implementor ::= T_implements type_ident?
 Field ::= VariableDecl | FunctionDecl
 
-InterfaceDecl ::= T_interface ident { Prototype* }
-Prototype ::= TypeVoid ident ( Formals ) 
+InterfaceDecl ::= T_interface type_ident { Prototype* }
+Prototype ::= Type ident ( Formals ) | T_void ident ( Formals ) 
 StmtBlock ::= { Stmt* }
 Stmt ::= Expr? ; | IfStmt | WhileStmt | ForStmt | \
  BreakStmt | ReturnStmt | PrintStmt | StmtBlock | VariableDecl
@@ -28,7 +29,7 @@ BreakStmt ::= T_break ;
 PrintStmt ::= T_Print ( Expr,+ ) ;
 
 Expr ::= Constant | LValue | T_this | Call | ( Expr ) | \
- - Expr | T_ReadInteger ( ) | T_ReadLine ( ) | T_New ident  | \
+ T_ReadInteger ( ) | T_ReadLine ( ) | T_New ident  | \
  T_NewArray ( Expr , Type ) 
 LValue ::= ident | Expr . ident | Expr [ Expr ]
 Call ::= ident ( Actuals ) | Expr . ident ( Actuals )
