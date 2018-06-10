afa10107ffbca1289be36de291bb85d2afbdab79
FluorineDog <fluorine@gmail.com>
2018-06-10 13:29:39 +0800

    init

--- parser/bnf.hs
+++ parser/bnf.hs
@@ -4,7 +4,7 @@ Decl ::= VariableDecl | FunctionDecl | ClassDecl | InterfaceDecl
 VariableDecl ::= Variable ;
 Variable ::= Type ident
 
-Type ::= T_int | T_double | T_bool | T_string | ident | Type [ ]
+Type ::= T_int | T_double | T_bool | T_string | TYPE_ID | Type [ ]
 TypeVoid ::= Type | T_void 
 FunctionDecl ::= TypeVoid ident ( Formals ) StmtBlock
 Formals ::= Variable,*
@@ -15,19 +15,19 @@ Field ::= VariableDecl | FunctionDecl
 
 InterfaceDecl ::= T_interface ident { Prototype* }
 Prototype ::= TypeVoid ident ( Formals ) 
-StmtBlock ::= { VariableDecl* Stmt* }
+StmtBlock ::= { Stmt* }
 Stmt ::= Expr? ; | IfStmt | WhileStmt | ForStmt | \
- BreakStmt | ReturnStmt | PrintStmt | StmtBlock 
+ BreakStmt | ReturnStmt | PrintStmt | StmtBlock | VariableDecl
 
 IfStmt ::=  T_if ( Expr ) Stmt ElseTail?
-ElseTail ::= T_else Expr
+ElseTail ::= T_else Stmt
 WhileStmt ::= T_while ( Expr ) Stmt
 ForStmt ::= T_for ( Expr? ; Expr ; Expr? ) Stmt
 ReturnStmt ::= T_return Expr? ;
 BreakStmt ::= T_break ;
 PrintStmt ::= T_Print ( Expr,+ ) ;
 
-Expr ::= Constant | LValue = Expr | LValue | T_this | Call | ( Expr ) | \
+Expr ::= Constant | LValue | T_this | Call | ( Expr ) | \
  - Expr | T_ReadInteger ( ) | T_ReadLine ( ) | T_New ident  | \
  T_NewArray ( Expr , Type ) 
 LValue ::= ident | Expr . ident | Expr [ Expr ]
