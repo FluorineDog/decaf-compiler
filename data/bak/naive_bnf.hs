09fe705e6a71f344475674a860d76df038a15c81
FluorineDog <fluorine@gmail.com>
2018-06-16 23:00:20 +0800

    no-gen

--- data/bak/naive_bnf.hs
+++
@@ -1,7 +0,0 @@
-Program ::= Stmt+
-Stmt ::= Expr? ; | PrintStmt
-Expr ::= Expr + Expr | NUM | Call
-NUM ::= NUM_int | NUM_double 
-Call ::= ident ( Actuals )
-Actuals ::= Expr,* 
-PrintStmt ::= Print ( Expr,+ )
--- kernel/generated/visitor_summary.h
+++
@@ -1,3 +0,0 @@
-#include "PrintVisitor.h"
-#include "LoadSymbolTableVisitor.h"
-#include "StaticAnalyseVisitor.h"
